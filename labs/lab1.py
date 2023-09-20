ba7daad241fd4404c9f744c800977dff90077ee5
ssoraki <144201904+ssoraki@users.noreply.github.com>
2023-09-20 11:11:36 +0500

    чето написала

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,4 +1,12 @@
 import math
 
-def task1():
-  pass
+print('введите x, y, z')
+x = float(input())
+y = float(input())
+z = float(input())
+
+a = ((pow((abs(x - 1)),1/5) + math.exp(-y)) / (math.sin(x) + math.log10(1 + y)))
+b = (math.sin(pow((abs(z + 71)),5)) + math.cos(pow(abs(y),3)) + pow(x,3) + pow(y,4))
+
+print('a: ',a)
+print('b: ',b)
