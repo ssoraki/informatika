9432c77bf72b9ecafb88129c26be7282ccf21953
ssoraki <144201904+ssoraki@users.noreply.github.com>
2023-10-04 11:12:06 +0500

    Update lab2.py

---
+++ labs/lab2.py
@@ -0,0 +1,17 @@
+def task5():
+
+    h = float(input('введите высоту в метрах'))
+    #v = float(input('введите начальную скорость'))
+
+    t = (2*h / 9.8)**0.5
+
+    print('время падения =', t, 'с')
+
+
+def task6():
+    
+    k = float(input('сторона треугольника = '))
+    alpha = float(input('угол альфа = '))
+    beta = float(input('угол бета = '))
+    
+   
